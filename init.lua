c2d6c467f34fa5ce08b1f3ba1d4a66d070f8baac
throwea <saeruig@saeruig>
2024-12-18 23:54:53 -0600

    reversed the searching keymaps

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@
 -- figure out how to get browser inside of tmux
 -- Learn more about helpful tmux plugins
 -- Find way to list all of the breakpoints 
+-- add the conf from here into the mux plugin https://github.com/omerxx/dotfiles/blob/master/tmux/tmux.conf
 -- Typescript lsp is not working
 -- download vim be good to improve vim skills
 -- Set a break point in a python project and run the debugger
@@ -15,7 +16,6 @@
 -- Figure out the tmux keymap
 -- Modularize config
 --  - Create a file for remaps
--- add remap for :Ex
 -- learn quickfix
 -- https://www.youtube.com/watch?v=-ybCiHPWKNA 1:00:00
 -- package/plugin management https://www.youtube.com/watch?v=-ybCiHPWKNA&t=1557s 
@@ -322,8 +322,8 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>p', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>sp', builtin.git_files, { desc = 'Search Git Files' })
+      vim.keymap.set('n', '<leader>sp', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>p', builtin.git_files, { desc = 'Search Git Files' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
