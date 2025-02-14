53ac739f54f6f88365ceaab45c3d89654cdbdf0a
throwea <saeruig@saeruig>
2025-02-14 00:34:56 -0600

    added mason dependencies

--- init.lua
+++ init.lua
@@ -10,6 +10,7 @@
 -- find way to get inlay hints working when in insert mode
 -- use vim be good and practice
 -- create a bash script that will completely install all the dev dependencies needed to get started
+--  - add .txt file explaining everything I need to do to get everything up and running
 --  - create a gitrepo around my config
 --  - find a way to include .zshrc, .tmux.conf and neovim config entirely inside a git repo so it can be version controlled
 --  - create bash script which installs all dev dependencies via homebrew
